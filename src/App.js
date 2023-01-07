4420ac52bf26fb46532fd3682a7861b1ae024366
Premkumar S <kumarsprm@gmail.com>
2023-01-08 02:25:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { MdMicExternalOff } from 'react-icons/md';
+import { MdMicExternalOn } from 'react-icons/md';
+import { RxSpeakerOff } from 'react-icons/rx';
+import { RxSpeakerLoud } from 'react-icons/rx';
+import { useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 function App() {
+  const [vol,setVol] = useState(1);
+  const speakContent = () => {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = document.getElementById('textbox').innerHTML;
+    msg.volume=vol;
+    window.speechSynthesis.speak(msg);
+  }
+  const {
+    transcript,
+    listening,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();  
+  const reset = () => {
+    window.location.reload();
+  }
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='volume'>
+      <RxSpeakerOff className='icon'/>
+        <input type="range" min="0" max="1" step="0.1" onChange={(e) => setVol(e.target.value)} />
+      <RxSpeakerLoud className='icon'/>
+      </div>
+      <div className='bclass'>
+        <button onClick={reset}>Reset</button>
+      </div>
+      
+      <div className="main">
+        <div className="left" onClick={listening ? SpeechRecognition.stopListening : SpeechRecognition.startListening }>
+          {
+            listening ? <MdMicExternalOn className='icon'/> : <MdMicExternalOff className='icon'/>
+          }
+          
+        </div>
+        
+        <div contentEditable className="textbox" id='textbox'>
+        {
+          transcript ? <p>{transcript}</p> : <p>Click Mic Button For Convert Your Voice to Text and
+          Click Speaker Button Reading The Contents of the Textbox</p>
+        }
+        </div>
+        <div className="right" onClick={speakContent}>
+          {
+            listening ? <RxSpeakerOff className='icon'/> : <RxSpeakerLoud className='icon'/>
+          }
+        
+        </div>
+      </div>
     </div>
   );
 }
